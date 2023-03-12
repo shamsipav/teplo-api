25b0dd6fa9c5849c9511a37fee0e7c68d0eac346
Павел Шамсимухаметов <spr@pkzdrav.ru>
2023-03-12 12:03:25 +0500

    feat<back> Create logic of calculate base period

---
+++ TeploAPI/Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using TeploAPI.Models;
+using TeploAPI.Services;
+using TeploAPI.ViewModels;
+
+namespace TeploAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Furnace furnace = Furnace.GetDefaultData();
+
+            CalculateService calculate = new CalculateService();
+
+            var calculateResult = calculate.СalculateThermalRegime(furnace);
+
+            var result = new ResultViewModel { Input = furnace, Result = calculateResult };
+
+            return Ok(result);
+        }
+    }
+}
---
+++ TeploAPI/ViewModels/ResultViewModel.cs
@@ -0,0 +1,13 @@
+﻿using TeploAPI.Models;
+
+namespace TeploAPI.ViewModels
+{
+    /// <summary>
+    /// Класс для отображение исходных данных и результатов расчета
+    /// </summary>
+    public class ResultViewModel
+    {
+        public Furnace? Input { get; set; }
+        public Result? Result { get; set; }
+    }
+}
