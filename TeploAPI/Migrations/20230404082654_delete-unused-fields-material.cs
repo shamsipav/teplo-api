b5dfae711fec82a3023efd0b65be32655d9c8216
Павел Шамсимухаметов <spr@pkzdrav.ru>
2023-04-04 13:27:30 +0500

    refactor(db): Миграция для удаления неиспользуемых полей

---
+++ TeploAPI/Migrations/20230404082654_delete-unused-fields-material.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeploAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class deleteunusedfieldsmaterial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Coal",
+                table: "Materials");
+
+            migrationBuilder.DropColumn(
+                name: "Share",
+                table: "Materials");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Coal",
+                table: "Materials",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Share",
+                table: "Materials",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
